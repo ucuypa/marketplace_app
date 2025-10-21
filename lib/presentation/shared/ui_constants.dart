a43b31b3b87ddb0df8336bebc6ef463c72502fd1
ahuaiqom <81741650+ahuaiqom@users.noreply.github.com>
2025-10-21 20:47:09 +0700

    style(tokens): centralize color palette and constants (kPrimary, kScaffoldBg, kText colors)

---
+++ lib/presentation/shared/ui_constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kPrimary = Color(0xFF5B9EE1);
+const kTextPrimary = Color(0xFF1A242F);
+const kTextMuted = Color(0xFF707B81);
+const kDanger = Color(0xFFF77165);
+const kScaffoldBg = Color(0xFFF8F9FA);
