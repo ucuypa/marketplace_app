d7e63c682eea9e411238d191d37cae308987669d
ahuaiqom <81741650+ahuaiqom@users.noreply.github.com>
2025-10-24 19:21:18 +0700

    TopBar change the location icon color from black to red

--- lib/presentation/home/widgets/top_bar.dart
+++ lib/presentation/home/widgets/top_bar.dart
@@ -10,31 +10,50 @@ class TopBar extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: dp(context, 20)),
       child: Row(
         children: [
-          _circleBtn(context,
-              child: Image.asset('assets/icon/Home.png',
-                  width: dp(context, 24),
-                  height: dp(context, 24),
-                  errorBuilder: (_, __, ___) =>
-                      Icon(Icons.home_outlined, size: dp(context, 22)))),
+          _circleBtn(
+            context,
+            child: Image.asset(
+              'assets/icon/Home.png',
+              width: dp(context, 24),
+              height: dp(context, 24),
+              errorBuilder: (_, __, ___) =>
+                  Icon(Icons.home_outlined, size: dp(context, 22)),
+            ),
+          ),
 
           SizedBox(width: dp(context, 16)),
           Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                Text('Location',
-                    style: inter(context, 12,
-                        w: FontWeight.w300, color: kTextMuted)),
+                Text(
+                  'Location',
+                  style: inter(
+                    context,
+                    12,
+                    w: FontWeight.w300,
+                    color: kTextMuted,
+                  ),
+                ),
                 SizedBox(height: dp(context, 2)),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    Icon(Icons.location_on,
-                        size: dp(context, 14), color: kTextPrimary),
+                    Icon(
+                      Icons.location_on,
+                      size: dp(context, 14),
+                      color: const Color.fromARGB(255, 248, 114, 101),
+                    ),
                     SizedBox(width: dp(context, 6)),
-                    Text('Brawijaya University',
-                        style:
-                            inter(context, 14, w: FontWeight.w500, color: kTextPrimary)),
+                    Text(
+                      'Brawijaya University ',
+                      style: inter(
+                        context,
+                        14,
+                        w: FontWeight.w500,
+                        color: kTextPrimary,
+                      ),
+                    ),
                   ],
                 ),
               ],
@@ -44,20 +63,26 @@ class TopBar extends StatelessWidget {
           Stack(
             clipBehavior: Clip.none,
             children: [
-              _circleBtn(context,
-                  child: Image.asset('assets/icon/AddCartTopRight.png',
-                      width: dp(context, 24),
-                      height: dp(context, 24),
-                      errorBuilder: (_, __, ___) =>
-                          Icon(Icons.favorite_border, size: dp(context, 22)))),
+              _circleBtn(
+                context,
+                child: Image.asset(
+                  'assets/icon/AddCartTopRight.png',
+                  width: dp(context, 24),
+                  height: dp(context, 24),
+                  errorBuilder: (_, __, ___) =>
+                      Icon(Icons.favorite_border, size: dp(context, 22)),
+                ),
+              ),
               Positioned(
                 right: dp(context, 6),
                 top: dp(context, 6),
                 child: Container(
                   width: dp(context, 8),
                   height: dp(context, 8),
-                  decoration:
-                      const BoxDecoration(color: kDanger, shape: BoxShape.circle),
+                  decoration: const BoxDecoration(
+                    color: kDanger,
+                    shape: BoxShape.circle,
+                  ),
                 ),
               ),
             ],
@@ -68,12 +93,12 @@ class TopBar extends StatelessWidget {
   }
 
   Widget _circleBtn(BuildContext ctx, {required Widget child}) => Container(
-        width: dp(ctx, 44),
-        height: dp(ctx, 44),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(dp(ctx, 44)),
-        ),
-        child: Center(child: child),
-      );
+    width: dp(ctx, 44),
+    height: dp(ctx, 44),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(dp(ctx, 44)),
+    ),
+    child: Center(child: child), 
+  );
 }
