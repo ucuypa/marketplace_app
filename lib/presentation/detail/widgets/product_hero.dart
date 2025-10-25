730f06e08555b620589a8baf647e53f317102d93
ahuaiqom <81741650+ahuaiqom@users.noreply.github.com>
2025-10-25 17:09:20 +0700

    Add ProductHero widget for displaying product images in detail view

---
+++ lib/presentation/detail/widgets/product_hero.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:marketplace_app/presentation/shared/scale.dart';
+import 'package:marketplace_app/presentation/shared/ui_constants.dart';
+
+class ProductHero extends StatelessWidget {
+  final String imageAsset;
+  const ProductHero({super.key, required this.imageAsset});
+
+  @override
+  Widget build(BuildContext context) => Padding(
+        padding: EdgeInsets.symmetric(horizontal: dp(context, 20)),
+        child: Container(
+          height: dp(context, 240),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(dp(context, 24)),
+          ),
+          child: Center(
+            child: Image.asset(
+              imageAsset,
+              height: dp(context, 180),
+              fit: BoxFit.contain,
+              errorBuilder: (_, __, ___) => Icon(Icons.image, size: dp(context, 40)),
+            ),
+          ),
+        ),
+      );
+}
