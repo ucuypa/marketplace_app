fd66cf8bf1b2c0533543d88eb254bc87393a293c
ahuaiqom <81741650+ahuaiqom@users.noreply.github.com>
2025-10-27 16:00:30 +0700

    Add CartList widget to display items in the shopping cart

---
+++ lib/presentation/cart/widgets/cart_list.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../presentation/shared/scale.dart';
+import '../../../presentation/shared/ui_constants.dart';
+import '../application/cart_controller.dart';
+import '../models/cart_item.dart';
+import 'cart_item_row.dart';
+
+class CartList extends StatelessWidget {
+  const CartList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Consumer<CartController>(
+      builder: (_, cart, __) {
+        if (cart.items.isEmpty) {
+          return Center(
+            child: Text('Your cart is empty',
+                style: inter(context, 14, color: kTextMuted)),
+          );
+        }
+        return ListView.separated(
+          padding: EdgeInsets.all(dp(context, 16)),
+          itemCount: cart.items.length,
+          separatorBuilder: (_, __) => SizedBox(height: dp(context, 12)),
+          itemBuilder: (_, i) => CartItemRow(item: cart.items[i]),
+        );
+      },
+    );
+  }
+}
